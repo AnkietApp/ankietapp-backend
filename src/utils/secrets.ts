b23e68bc698640cac1d7895b651186b2a414df19
sochacki111 <michalsochacki111@gmail.com>
2020-11-29 12:36:55 +0100

    Feature finished.
    Add sending emails to users on survey creation. Created integration with mailgun

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -1,1 +1,14 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+if (fs.existsSync('.env')) {
+  console.log('Using .env file to supply config environment variables');
+  dotenv.config({ path: '.env' });
+} else {
+  dotenv.config({ path: '.env.example' });
+}
+
 export const JWT_SECRET = process.env.JWT_SECRET || 'somesecrettoken';
+
+export const MAILGUN_API_KEY = String(process.env.MAILGUN_API_KEY);
+export const MAILGUN_DOMAIN = String(process.env.MAILGUN_DOMAIN);
