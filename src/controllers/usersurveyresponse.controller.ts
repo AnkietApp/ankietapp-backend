e0e5269c9cfe94a33aa516ac70819b845341ff14
sochacki111 <michalsochacki111@gmail.com>
2020-12-01 15:20:45 +0100

    Apply formatting

--- src/controllers/usersurveyresponse.controller.ts
+++ src/controllers/usersurveyresponse.controller.ts
@@ -6,12 +6,12 @@ import UserSurveyResponse from '../entity/UserSurveyResponse';
 import Answer from '../entity/Answer';
 
 export const updateSurveyResponse = async (
-    req: Request,
-    res: Response
+  req: Request,
+  res: Response
 ): Promise<Response> => {
-    const user = (<any>req).user;
-    try {
-        const userResponse = await getRepository(UserSurveyResponse)
+  const user = (<any>req).user;
+  try {
+    const userResponse = await getRepository(UserSurveyResponse)
       .createQueryBuilder('userSurveyResponse')
       .select()
       .where('userSurveyResponse.id = :userResponseId')
@@ -23,18 +23,24 @@ export const updateSurveyResponse = async (
         completed: false
       })
       .getOne();
-        getRepository(UserSurveyResponse).update(userResponse!.id, {...userResponse!, completed: true});
-        const updatedResponse = await getRepository(UserSurveyResponse).findOne({ where: {id: req.params.id, userId: user.id, completed: true}});
-        console.log(updatedResponse);
+    getRepository(UserSurveyResponse).update(userResponse!.id, {
+      ...userResponse!,
+      completed: true
+    });
+    const updatedResponse = await getRepository(UserSurveyResponse).findOne({
+      where: { id: req.params.id, userId: user.id, completed: true }
+    });
+    console.log(updatedResponse);
 
-        req.body.answers.forEach(async (answer: Answer) => {
-            const newAnswer: Answer = getRepository(Answer).create({ ...answer, userSurveyResponseId: updatedResponse!.id });
-            await getRepository(Answer).save(newAnswer);
-        });
-        return res.json(updatedResponse);
-        
-    }
-    catch (err) {
-        return res.send(err);
-    }
+    req.body.answers.forEach(async (answer: Answer) => {
+      const newAnswer: Answer = getRepository(Answer).create({
+        ...answer,
+        userSurveyResponseId: updatedResponse!.id
+      });
+      await getRepository(Answer).save(newAnswer);
+    });
+    return res.json(updatedResponse);
+  } catch (err) {
+    return res.send(err);
+  }
 };
